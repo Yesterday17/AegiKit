ee9e07dd35b50dfd97a4abb51f342c312a5bc886
Yesterday17 <t@yesterday17.cn>
2020-06-25 17:31:17 +0800

    feat: highlight for karaoke template

--- src/services/formatLua.ts
+++ src/services/formatLua.ts
@@ -1,25 +1,22 @@
-import * as vscode from "vscode";
 import * as fmt from "lua-fmt";
+import * as vscode from "vscode";
 
 export default function (context: vscode.ExtensionContext) {
-  vscode.languages.registerDocumentFormattingEditProvider(
-    { language: "lua", scheme: "*" },
-    {
-      async provideDocumentFormattingEdits(
-        document: vscode.TextDocument,
-        options: vscode.FormattingOptions,
-        token: vscode.CancellationToken
-      ): Promise<vscode.TextEdit[]> {
-        return [
-          new vscode.TextEdit(
-            document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)),
-            fmt.formatText(document.getText(), {
-              useTabs: !options.insertSpaces,
-              indentCount: options.tabSize,
-            })
-          ),
-        ];
-      },
-    }
-  );
+  vscode.languages.registerDocumentFormattingEditProvider("lua", {
+    async provideDocumentFormattingEdits(
+      document: vscode.TextDocument,
+      options: vscode.FormattingOptions,
+      token: vscode.CancellationToken
+    ): Promise<vscode.TextEdit[]> {
+      return [
+        new vscode.TextEdit(
+          document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)),
+          fmt.formatText(document.getText(), {
+            useTabs: !options.insertSpaces,
+            indentCount: options.tabSize,
+          })
+        ),
+      ];
+    },
+  });
 }
